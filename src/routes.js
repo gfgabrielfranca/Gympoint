5bf4ca62e074d787a5ef52a6092206dd1ecd1a45
Gabriel França <dev.gabrielfranca@gmail.com>
2019-11-06 21:02:47 -0300

    implementing students

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import StudentsController from './app/controllers/StudentsController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -12,6 +13,7 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.post('/students', StudentsController.store);
 routes.put('/users', UserController.update);
 
 export default routes;
